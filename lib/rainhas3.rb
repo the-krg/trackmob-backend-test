8989acc207fbe07650767b15c8464f1a17d5a771
Enzo Vinicius Haguiwara <enzo_vinicius@hotmail.com>
2017-11-17 12:06:10 -0200

    Fix: Scan funcionava apenas para 4 rainhas, deixei uma variavel que aumenta conforme o numero de rainhas

--- lib/rainhas3.rb
+++ lib/rainhas3.rb
@@ -1,33 +1,36 @@
 def line_available?(board, lin, col, n)
-  for check_column in 0..col
+  col.times do |check_column|
     return false if board[[lin, check_column]] == '* '
   end
+
   1.upto(n) do |j|
     return false if board[[lin+j, col+j]] == '* ' || board[[lin-j, col-j]] == '* '
     return false if board[[lin-j, col+j]] == '* ' || board[[lin+j, col-j]] == '* '
   end
+
   true
 end
 
 def n_queens?(board, column, n)
-  return true if column == n
-   #Fim
-  for line in 0..n-1
+  return true if column == n  #Fim
+  n.times do |line|
     if line_available?(board, line, column, n)
       board[[line, column]] = '* '
       return true if n_queens?(board, column + 1, n) #Próximo
       board[[line, column]] = '- '
     end
   end
+
   false
 end
 
 def generate_board(n, board)
-  for lins in 0..n-1
-    for cols in 0..n-1
+# ----------> nao usar for
+  n.times do |lins|
+    n.times do |cols|
       board[[lins, cols]] = '- '
     end
-  end
+  end#                ----------> ainda assim, uso de 2 loops
   board
 end
 
@@ -36,8 +39,20 @@ board = Hash.new
 puts "Diga um número(entre 4 e 8):"
 n = gets.to_i
 
-generate_board(n, board)
-
-if n_queens?(board, 0, n)
-  puts board.values.join.scan(/......../)
+dots = ""
+n.times do
+  dots << ".."
 end
+
+generate_board(n, board)
+if n.between?(4, 8) 
+  if n_queens?(board, 0, n)
+    puts board.values.join.scan(/#{dots}/)
+  end
+else
+  puts "Valor digitado inválido."
+end
+
+
+
+
